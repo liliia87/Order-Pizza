bc7742908518fed80e5021007458956c3a026fc3
liliia87 <lilia.krivelyova@gmail.com>
2022-05-08 00:20:04 -0700

    Work on adding prototype calculateTheCost

---
+++ README.md
@@ -0,0 +1,39 @@
+# _Pizza Parlor_
+
+#### Authored by: _**Liliia Kryvelova**_
+
+https://github.com/liliia87/project3.git
+
+#### _An application that takes the number from the user and returnsa range of numbers from 0 to the user inputted number with the certain subtitutions._
+
+## Technologies Used
+*_Html_
+*_JQuery_
+*_JavaScript_
+
+## Description
+
+_The web applications has a user input, where user can insert the number for the length of array;_
+_Next step is "Click" on the button;_
+_The web application will return a range of numbers from 0 to the user's inputted number with the replaced numbers._
+
+## Setup/Installation Requirements
+
+*_Clone this git on your local computer_
+*_For clonning: use button Clone -> from repository._
+*_Copy the clone command(SSH format or HTTPS)._
+*_From a terminal on your local computer, use command "cd" to find the directory where you want to clone this repository._
+*_Type the command ($ git clone HTTPs)._
+*_If you clone successfully , a new sub-directory appears on your local computer._
+*_In the directory you will find(index.html, folder->css, folder->js)._
+*_Run the index.html (double click on it and the page should appear in web browser)._
+*_If you are willing to debug or change something in the project, the best way is to open it through the VisuaStudio._
+
+
+## Known Bugs
+
+*_For now, there aren't known bugs_
+
+## License
+
+_contact lilia.krivelyova@gmail.com_
--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ function Pizza(){
   this.type;
   this.size;
   this.cost;
+  this.toppings=[];
 }
 
 let order = [];
@@ -14,6 +15,11 @@ Pizza.prototype.calculateCost = function (){
     ['Sicilian Pizza', 8.50],
     ['Californian Pizza', 6.50]
   ]);
+  let topp = new Map([
+    ['pineapple', 1],
+    ['anchovies', 0.5]
+  ]);
+
   let p = this.type[0].trim();
   this.cost = tp.get(p);
 
@@ -21,31 +27,46 @@ Pizza.prototype.calculateCost = function (){
   
   switch(sp[1].trim()) {
     case 'Small':
-      this.cost = this.cost + 1;
+      this.cost = this.cost + 0;
       break;
     case 'Medium':
-      this.cost = this.cost + 1.50;
+      this.cost = this.cost + 1;
       break;
     case 'Large':
-      this.cost += 2;
+      this.cost += 1.50;
       break;
     case 'Fiesta':
-      this.cost +=2.50;
+      this.cost += 2;
       break;
 
   }
-
+  for(let i=0; i<this.toppings.length;i++)
+    this.cost += topp.get(this.toppings[i]);
   return this.cost;
 }
+
+Pizza.prototype.toppingsToString = function(){
+  let result = "";
+  for(let i=0;i<this.toppings.length;i++){
+    result+=this.toppings[i]+" ";
+  }
+  if(result == ""){
+    return "None";
+  }
+  return result;
+}
+
 
 $(document).ready(function(){
   $(".description").html("");
   $('select').change(function(event){
 
     event.preventDefault();
+    
     $(".description").html("<mark>"+"You selected: "+"</mark>" + $("#type-pizza").val()+"<br>");
     $(".description").append("<mark>"+"The size of pizza is: "+"</mark>" + $("#size-pizza").val());
   });
+
   let k=0;
 
   $("#set-choose").click(function(event){
@@ -55,11 +76,19 @@ $(document).ready(function(){
     let pizza = new Pizza();
     pizza.type = $('#type-pizza').val().split("~");
     pizza.size = $('#size-pizza').val().split('~');
+    $.each($("input[name='toppings']:checked"), function(){
+      pizza.toppings.push(this.id);
+    });
     pizza.cost = pizza.calculateCost();
+    
+    
+  
+
     
 
     order.push(pizza);
-    $("#row-bottom").append(k+"." +`${pizza.type[0]}`+`${pizza.size}`+": " + `${pizza.cost}`+"$"+"<hr>")
+    $("#row-bottom").append(k+"." +`${pizza.type[0]}`+`${pizza.size}`+": " + `${pizza.cost}`+"$<br>")
+    $("#row-bottom").append("Your toppings: " + pizza.toppingsToString()+"<hr>");
 
   });
 
